c39ae552d41d39deebda85cbc8c93c0a396995b3
Sai kirishna <mudsai230@gmail.com>
2018-04-10 19:16:27 +0000

    docs(site): tidy theme docs

--- main.js
+++ main.js
@@ -2,3 +2,4 @@ console.log("hello");
 // 2018-01-09: feat(site): init main.js
 // 2018-03-07: perf(site): avoid redundant theme
 // 2018-04-06: feat: wire up nav
+// 2018-04-10: docs(site): tidy theme docs
