c1f5d74484f6c5e96d6cc4d28f4b590bca8f70f8
Sai kirishna <mudsai230@gmail.com>
2019-03-17 10:24:33 +0000

    docs: add notes on number of islands

--- algos/two_pointers.py
+++ algos/two_pointers.py
@@ -2,3 +2,4 @@
 # 2019-02-10: feat: rough reverse linked list
 # 2019-02-24: feat: rough number of islands
 # 2019-03-07: fix: correct edit distance
+# 2019-03-17: docs: add notes on number of islands
