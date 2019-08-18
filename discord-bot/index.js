bc8f49436103c02aaaf386ccd0a33dc804b617b0
Sai kirishna <mudsai230@gmail.com>
2019-08-18 23:21:56 +0000

    fix(bot): avoid slash commands

--- discord-bot/index.js
+++ discord-bot/index.js
@@ -1,3 +1,4 @@
 // 2019-05-04: feat(bot): init index.js
 // 2019-05-17: feat(bot): first pass at cooldowns
 // 2019-05-17: refactor(bot): rename slash commands
+// 2019-08-18: fix(bot): avoid slash commands
