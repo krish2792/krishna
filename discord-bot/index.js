9ae8d12882b0abf3696cb02549e6f40f79247582
Sai kirishna <mudsai230@gmail.com>
2019-10-03 17:12:25 +0000

    chore: update role assignment config

--- discord-bot/index.js
+++ discord-bot/index.js
@@ -2,3 +2,4 @@
 // 2019-05-17: feat(bot): first pass at cooldowns
 // 2019-05-17: refactor(bot): rename slash commands
 // 2019-08-18: fix(bot): avoid slash commands
+// 2019-10-03: chore: update role assignment config
