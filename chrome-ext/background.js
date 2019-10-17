c09abee043fbf669f54f226b86b2680945e0a555
Sai kirishna <mudsai230@gmail.com>
2019-10-17 15:22:07 +0000

    feat(ext): introduce manifest v3

--- chrome-ext/background.js
+++ chrome-ext/background.js
@@ -11,3 +11,4 @@
 // 2019-08-10: style(ext): prettier pass
 // 2019-09-05: feat(ext): introduce manifest v3
 // 2019-09-20: test(ext): cover popup edge case
+// 2019-10-17: feat(ext): introduce manifest v3
