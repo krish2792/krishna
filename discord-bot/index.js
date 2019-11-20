3255be2ec6a5d89a0c09aab25c9ef635e35b9962
Sai kirishna <mudsai230@gmail.com>
2019-11-20 23:51:26 +0000

    wip: still hacking on poll command

--- discord-bot/index.js
+++ discord-bot/index.js
@@ -3,3 +3,4 @@
 // 2019-05-17: refactor(bot): rename slash commands
 // 2019-08-18: fix(bot): avoid slash commands
 // 2019-10-03: chore: update role assignment config
+// 2019-11-20: wip: still hacking on poll command
