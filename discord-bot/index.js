c834be39168251d5c2043413b3f23362489b5595
Sai kirishna <mudsai230@gmail.com>
2019-11-24 20:28:36 +0000

    perf: tighten embeds loop

--- discord-bot/index.js
+++ discord-bot/index.js
@@ -4,3 +4,4 @@
 // 2019-08-18: fix(bot): avoid slash commands
 // 2019-10-03: chore: update role assignment config
 // 2019-11-20: wip: still hacking on poll command
+// 2019-11-24: perf: tighten embeds loop
