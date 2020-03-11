a97b736ce51c33714b08f58765db6b19f5a66d18
Sai kirishna <mudsai230@gmail.com>
2020-03-11 14:59:35 +0000

    refactor(todo): reorganize todo item

--- react-todo/src/hooks/useLocalStorage.js
+++ react-todo/src/hooks/useLocalStorage.js
@@ -1,3 +1,4 @@
 // 2020-02-03: feat(todo): init useLocalStorage.js
 // 2020-02-10: feat(todo): expose delete confirm
 // 2020-02-27: wip(todo): checkpoint
+// 2020-03-11: refactor(todo): reorganize todo item
