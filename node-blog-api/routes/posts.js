08376819fa7e0573abde62f48f440be2d92e0a55
Sai kirishna <mudsai230@gmail.com>
2020-03-24 23:09:23 +0000

    test(api): parametrize auth route tests

--- node-blog-api/routes/posts.js
+++ node-blog-api/routes/posts.js
@@ -1,3 +1,4 @@
 // 2020-01-23: feat(api): init posts.js
 // 2020-02-01: perf(api): cache rate limit
 // 2020-03-15: style(api): reflow comment endpoint
+// 2020-03-24: test(api): parametrize auth route tests
