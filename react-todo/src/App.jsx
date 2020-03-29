6f8a8a0dc27f381eaf02f5d3d7ef3c9adf4a75f8
Sai kirishna <mudsai230@gmail.com>
2020-03-29 21:28:18 +0000

    fix(todo): unbreak keyboard shortcuts

--- react-todo/src/App.jsx
+++ react-todo/src/App.jsx
@@ -4,3 +4,4 @@
 // 2020-02-08: style: reflow todo list
 // 2020-02-17: refactor(todo): extract edit inline
 // 2020-02-17: refactor: split localStorage persistence
+// 2020-03-29: fix(todo): unbreak keyboard shortcuts
