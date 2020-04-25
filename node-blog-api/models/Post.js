846e7232b986b097cae53ab84c1b92289d1b69e6
Sai kirishna <mudsai230@gmail.com>
2020-04-25 21:25:04 +0000

    refactor: reorganize comment endpoint

--- node-blog-api/models/Post.js
+++ node-blog-api/models/Post.js
@@ -2,3 +2,4 @@
 // 2020-03-08: refactor: reorganize auth route
 // 2020-03-30: fix(api): patch user model
 // 2020-03-31: fix(api): unbreak mongoose schema
+// 2020-04-25: refactor: reorganize comment endpoint
