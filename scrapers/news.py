64a5529518be2f2f9fe92eb70d13973edcf54ec0
Sai kirishna <mudsai230@gmail.com>
2020-04-25 14:16:36 +0000

    perf(scraper): avoid redundant requests session

--- scrapers/news.py
+++ scrapers/news.py
@@ -2,3 +2,4 @@
 # 2020-02-02: wip(scraper): still hacking on user-agent rotation
 # 2020-02-02: perf(scraper): tighten rate limit sleep loop
 # 2020-03-15: late night fix
+# 2020-04-25: perf(scraper): avoid redundant requests session
