d448ba850822a32e862840195edceaabae4d4270
Sai kirishna <mudsai230@gmail.com>
2020-05-10 22:34:58 +0000

    forgot a file

--- node-blog-api/middleware/auth.js
+++ node-blog-api/middleware/auth.js
@@ -2,3 +2,4 @@
 // 2020-02-15: feat(api): rough error middleware
 // 2020-04-04: docs(api): expand cors config section
 // 2020-05-07: fix(api): unbreak error middleware
+// 2020-05-10: forgot a file
