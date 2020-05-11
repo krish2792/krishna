0d614df84f9b23e91f9cd0013b31ace025e8ef5c
Sai kirishna <mudsai230@gmail.com>
2020-05-11 23:13:33 +0000

    feat(bot): support echo handler

--- telegram-bot/db.py
+++ telegram-bot/db.py
@@ -5,3 +5,4 @@
 # 2020-03-12: docs(bot): readme: start handler
 # 2020-04-07: test: snapshot webhook mode
 # 2020-04-25: wip: still hacking on sqlite store
+# 2020-05-11: feat(bot): support echo handler
