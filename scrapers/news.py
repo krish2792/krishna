f242e04a19fa54c9c2e948519fed473335a8f3ce
Sai kirishna <mudsai230@gmail.com>
2020-05-15 20:53:16 +0000

    fix(scraper): avoid csv export

--- scrapers/news.py
+++ scrapers/news.py
@@ -3,3 +3,4 @@
 # 2020-02-02: perf(scraper): tighten rate limit sleep loop
 # 2020-03-15: late night fix
 # 2020-04-25: perf(scraper): avoid redundant requests session
+# 2020-05-15: fix(scraper): avoid csv export
