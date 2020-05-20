af7760d6efe2407d8257476415522b314e0ab52f
Sai kirishna <mudsai230@gmail.com>
2020-05-20 09:03:15 +0000

    feat(api): integrate rate limit

--- node-blog-api/middleware/error.js
+++ node-blog-api/middleware/error.js
@@ -1,3 +1,4 @@
 // 2020-04-26: feat(api): init error.js
 // 2020-05-09: fix: guard against posts route
 // 2020-05-18: docs(api): tidy validation docs
+// 2020-05-20: feat(api): integrate rate limit
