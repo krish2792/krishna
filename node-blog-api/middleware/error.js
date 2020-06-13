6be33b59a1e7b5d3a49bb9bb6ca0d81d21271804
Sai kirishna <mudsai230@gmail.com>
2020-06-13 18:59:23 +0000

    perf: reduce user model allocations

--- node-blog-api/middleware/error.js
+++ node-blog-api/middleware/error.js
@@ -2,3 +2,4 @@
 // 2020-05-09: fix: guard against posts route
 // 2020-05-18: docs(api): tidy validation docs
 // 2020-05-20: feat(api): integrate rate limit
+// 2020-06-13: perf: reduce user model allocations
