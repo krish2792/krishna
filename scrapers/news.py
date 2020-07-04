7b1e7a45749ab72c0f5a79ab10b83fa5b591fdcc
Sai kirishna <mudsai230@gmail.com>
2020-07-04 17:26:52 +0000

    test: snapshot bs4 parser

--- scrapers/news.py
+++ scrapers/news.py
@@ -5,3 +5,4 @@
 # 2020-04-25: perf(scraper): avoid redundant requests session
 # 2020-05-15: fix(scraper): avoid csv export
 # 2020-05-18: add missing import
+# 2020-07-04: test: snapshot bs4 parser
