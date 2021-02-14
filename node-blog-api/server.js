345eaa241060804e660a92548a5c8c536f22eab8
Sai kirishna <mudsai230@gmail.com>
2021-02-14 13:53:01 +0000

    test: add tests for auth route

--- node-blog-api/server.js
+++ node-blog-api/server.js
@@ -15,3 +15,4 @@
 // 2020-12-23: style(api): consistent spacing
 // 2020-12-27: docs(api): document auth route
 // 2021-01-30: feat(api): integrate post model
+// 2021-02-14: test: add tests for auth route
