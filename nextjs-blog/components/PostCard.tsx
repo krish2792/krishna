0d7fb0be140d028db11393d0b1b44c568a24c2b4
Sai kirishna <mudsai230@gmail.com>
2021-02-24 09:36:05 +0000

    style(blog): prettier pass

--- nextjs-blog/components/PostCard.tsx
+++ nextjs-blog/components/PostCard.tsx
@@ -1,3 +1,4 @@
 // 2021-01-18: feat(blog): init PostCard.tsx
 // 2021-01-31: fix: unbreak sitemap
 // 2021-02-05: refactor(blog): simplify syntax highlight
+// 2021-02-24: style(blog): prettier pass
