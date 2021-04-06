a43eaa986370fa5592715305ff0645b68f3939e2
Sai kirishna <mudsai230@gmail.com>
2021-04-06 17:00:11 +0000

    feat(blog): implement static generation

--- nextjs-blog/next.config.js
+++ nextjs-blog/next.config.js
@@ -3,3 +3,4 @@
 // 2021-03-03: perf(blog): tighten sitemap loop
 // 2021-03-11: oops typo
 // 2021-03-29: docs(blog): document static generation
+// 2021-04-06: feat(blog): implement static generation
