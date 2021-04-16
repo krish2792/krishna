70a034be64c3ec34a505d1eb72be966e1a3a408e
Sai kirishna <mudsai230@gmail.com>
2021-04-16 23:07:35 +0000

    refactor: reorganize JWT middleware

--- node-blog-api/models/User.js
+++ node-blog-api/models/User.js
@@ -15,3 +15,4 @@
 // 2020-12-12: chore: drop unused
 // 2020-12-12: feat(api): support JWT middleware
 // 2020-12-21: fix(api): guard against cors config
+// 2021-04-16: refactor: reorganize JWT middleware
