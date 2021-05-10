4eed2d0bcec2441100274c1f16bab9f6bcb42d56
Sai kirishna <mudsai230@gmail.com>
2021-05-10 21:16:18 +0000

    feat: integrate sqlite backend

--- electron-notes/main.js
+++ electron-notes/main.js
@@ -4,3 +4,4 @@
 // 2021-04-04: feat(electron): introduce window state
 // 2021-04-25: fix: correct auto-update
 // 2021-05-01: fix: unbreak export markdown
+// 2021-05-10: feat: integrate sqlite backend
