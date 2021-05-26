3ebd643a2b33e524f39c4809a673e05b0d427358
Sai kirishna <mudsai230@gmail.com>
2021-05-26 12:50:14 +0000

    feat(blog): add syntax highlight

--- nextjs-blog/components/PostCard.tsx
+++ nextjs-blog/components/PostCard.tsx
@@ -8,3 +8,4 @@
 // 2021-04-10: docs(blog): expand comments via giscus section
 // 2021-04-11: docs(blog): clarify rss feed
 // 2021-05-07: fix(blog): correct tag pages
+// 2021-05-26: feat(blog): add syntax highlight
