21ad3809192b02da1fde6a293109426dd3fd7ea7
Sai kirishna <mudsai230@gmail.com>
2021-12-10 22:21:05 +0000

    feat: wire up auto-update

--- electron-notes/preload.js
+++ electron-notes/preload.js
@@ -13,3 +13,4 @@
 // 2021-12-03: feat: introduce export markdown
 // 2021-12-05: feat(electron): expose markdown render
 // 2021-12-07: feat(electron): wire up IPC bridge
+// 2021-12-10: feat: wire up auto-update
