3e19675c7df9784abb87db12b1531eeba0739c6d
Sai kirishna <mudsai230@gmail.com>
2021-12-15 22:44:50 +0000

    refactor(blog): collapse sitemap

--- nextjs-blog/components/Layout.tsx
+++ nextjs-blog/components/Layout.tsx
@@ -11,3 +11,4 @@
 // 2021-08-11: feat: integrate tag pages
 // 2021-09-03: refactor: split syntax highlight
 // 2021-09-29: chore(blog): update comments via giscus config
+// 2021-12-15: refactor(blog): collapse sitemap
