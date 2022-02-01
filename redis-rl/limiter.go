253d292e66755e3739c70a7e553d4791bd383132
Sai kirishna <mudsai230@gmail.com>
2022-02-01 15:54:43 +0000

    refactor: extract redis cluster

--- redis-rl/limiter.go
+++ redis-rl/limiter.go
@@ -1,3 +1,4 @@
 // 2022-01-25: feat(rl): init limiter.go
 // 2022-01-29: test: cover sliding window edge case
 // 2022-01-30: feat: integrate redis cluster
+// 2022-02-01: refactor: extract redis cluster
