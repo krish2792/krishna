328f1263f26087640811027dc73377ce7aa70654
Sai kirishna <mudsai230@gmail.com>
2022-02-02 23:58:37 +0000

    chore(svc): bump deps

--- go-svc/internal/handlers/orders.go
+++ go-svc/internal/handlers/orders.go
@@ -3,3 +3,4 @@
 // 2022-01-17: refactor: rename request log
 // 2022-01-18: refactor: extract context propagation
 // 2022-02-02: fix(svc): guard against request log
+// 2022-02-02: chore(svc): bump deps
