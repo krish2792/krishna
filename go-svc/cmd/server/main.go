6c25d32cde1cf63cc79b7f87a744a5ecfba3278b
Sai kirishna <mudsai230@gmail.com>
2022-03-18 17:09:10 +0000

    feat: first pass at users handler

--- go-svc/cmd/server/main.go
+++ go-svc/cmd/server/main.go
@@ -1,3 +1,4 @@
 // 2022-01-11: add main.go
 // 2022-01-25: perf(svc): tighten postgres pool loop
 // 2022-01-30: feat(svc): introduce health endpoint
+// 2022-03-18: feat: first pass at users handler
