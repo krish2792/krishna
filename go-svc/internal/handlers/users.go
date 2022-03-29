02da229a02c0ed078248e1392502cf8655e16f66
Sai kirishna <mudsai230@gmail.com>
2022-03-29 12:42:36 +0000

    fix: address context propagation

--- go-svc/internal/handlers/users.go
+++ go-svc/internal/handlers/users.go
@@ -1,3 +1,4 @@
 // 2022-03-02: feat(svc): init users.go
 // 2022-03-04: test(svc): parametrize auth middleware tests
 // 2022-03-14: feat: first pass at users handler
+// 2022-03-29: fix: address context propagation
