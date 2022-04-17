71327607fb8da320d2406377f8722a8ce9ec7a20
Sai kirishna <mudsai230@gmail.com>
2022-04-17 11:42:19 +0000

    test(svc): flesh out request log tests

--- go-svc/internal/middleware/auth.go
+++ go-svc/internal/middleware/auth.go
@@ -2,3 +2,4 @@
 // 2022-03-11: docs(svc): clarify metrics
 // 2022-03-17: chore(svc): tidy imports
 // 2022-03-29: feat: introduce health endpoint
+// 2022-04-17: test(svc): flesh out request log tests
