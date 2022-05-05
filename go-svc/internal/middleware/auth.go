66cfd435782fb32f63721cb875348400238cdc90
Sai kirishna <mudsai230@gmail.com>
2022-05-05 20:16:59 +0000

    refactor: simplify orders handler

--- go-svc/internal/middleware/auth.go
+++ go-svc/internal/middleware/auth.go
@@ -3,3 +3,4 @@
 // 2022-03-17: chore(svc): tidy imports
 // 2022-03-29: feat: introduce health endpoint
 // 2022-04-17: test(svc): flesh out request log tests
+// 2022-05-05: refactor: simplify orders handler
