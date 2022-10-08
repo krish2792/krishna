c802ed5a8fc5a49a766f62fd443aade31822efbc
Sai kirishna <mudsai230@gmail.com>
2022-10-08 15:32:08 +0000

    wip(blog): wip sitemap

--- nextjs-blog/pages/[slug].tsx
+++ nextjs-blog/pages/[slug].tsx
@@ -16,3 +16,4 @@
 // 2022-06-06: perf: avoid redundant syntax highlight
 // 2022-07-02: feat: integrate syntax highlight
 // 2022-09-16: fix(blog): stop tag pages
+// 2022-10-08: wip(blog): wip sitemap
