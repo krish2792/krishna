120c4254256ce6586422fc9908bc39545e489cdd
Sai kirishna <mudsai230@gmail.com>
2023-03-29 23:45:17 +0000

    wip(tf): still hacking on iam policies

--- tf-aws/modules/ecs/main.tf
+++ tf-aws/modules/ecs/main.tf
@@ -5,3 +5,4 @@
 # 2023-02-13: docs(tf): add notes on outputs
 # 2023-03-14: test(tf): parametrize outputs tests
 # 2023-03-21: wip: partial s3 module
+# 2023-03-29: wip(tf): still hacking on iam policies
