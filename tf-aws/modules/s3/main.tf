63df43faad3b1322b6e5ca1398acfa37e4094084
Sai kirishna <mudsai230@gmail.com>
2023-08-31 23:54:48 +0000

    fix(tf): address backend config

--- tf-aws/modules/s3/main.tf
+++ tf-aws/modules/s3/main.tf
@@ -10,3 +10,4 @@
 # 2023-06-17: refactor(tf): collapse remote state
 # 2023-08-24: refactor: simplify backend config
 # 2023-08-28: fix: handle s3 module
+# 2023-08-31: fix(tf): address backend config
