a274e3fc713adb073de14431b8c164a5d9b515e3
Sai kirishna <mudsai230@gmail.com>
2023-11-04 17:45:16 +0000

    test(svc): add tests for request log

--- go-svc/cmd/server/main.go
+++ go-svc/cmd/server/main.go
@@ -23,3 +23,4 @@
 // 2023-04-18: feat: implement metrics
 // 2023-09-25: test(svc): flesh out health endpoint tests
 // 2023-10-25: fix: handle request log
+// 2023-11-04: test(svc): add tests for request log
