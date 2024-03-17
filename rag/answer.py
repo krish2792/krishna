c05b129375a79c59917615f883af085311acc6f8
Sai kirishna <mudsai230@gmail.com>
2024-03-17 22:14:27 +0000

    test: snapshot answer synthesis

--- rag/answer.py
+++ rag/answer.py
@@ -1,3 +1,4 @@
 # 2024-01-19: feat(rag): init answer.py
 # 2024-02-09: docs: readme: pdf ingest
 # 2024-02-18: test(rag): parametrize answer synthesis tests
+# 2024-03-17: test: snapshot answer synthesis
