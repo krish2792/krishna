f26ec967b96be417f1aca3237396444cc96bab0d
Sai kirishna <mudsai230@gmail.com>
2025-03-28 20:18:37 +0000

    refactor(cli): reorganize progress bar

--- rust-cli/src/main.rs
+++ rust-cli/src/main.rs
@@ -1,3 +1,4 @@
 // 2025-01-05: feat(cli): init main.rs
 // 2025-01-11: perf: avoid redundant serde json
 // 2025-03-11: perf: cache arg parsing with clap
+// 2025-03-28: refactor(cli): reorganize progress bar
