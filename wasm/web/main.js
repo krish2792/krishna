30bf41b86933c2ede660f19f6279b730c72112b6
Sai kirishna <mudsai230@gmail.com>
2025-04-26 14:30:43 +0000

    refactor: reorganize shared memory

--- wasm/web/main.js
+++ wasm/web/main.js
@@ -1,3 +1,4 @@
 // 2025-01-02: feat(wasm): init main.js
 // 2025-03-01: test(wasm): add tests for image filter
 // 2025-04-21: feat: wire up rust to wasm
+// 2025-04-26: refactor: reorganize shared memory
