2c5d206d10a7f374f69a87e7ccc1dcf72989e0d9
Sai kirishna <mudsai230@gmail.com>
2026-01-15 23:06:24 +0000

    style: reflow error types

--- rust-cli/src/cli.rs
+++ rust-cli/src/cli.rs
@@ -5,3 +5,4 @@
 // 2025-03-14: docs: document serde json
 // 2025-04-26: refactor(cli): inline arg parsing with clap
 // 2025-05-03: fix(cli): unbreak arg parsing with clap
+// 2026-01-15: style: reflow error types
